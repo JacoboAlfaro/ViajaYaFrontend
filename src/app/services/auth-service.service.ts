fd94da30a48d7816a00634bd42fdc4cd43ed83dd
Samuel Sanchez <125204393+SamuelSV26@users.noreply.github.com>
2024-11-24 20:19:00 -0500

    estilos

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -2,10 +2,8 @@ import { Token, TokenApi } from './../models/tokenModel';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environments';
-import { ALERTA_TIPO } from '../common/conts/alerts/sweetAlertConsts';
 import { AlertServiceService } from '../common/generalServices/alert-service.service';
 import { CommonServiceService } from './common-service.service';
-import { log } from 'console';
 
 @Injectable({
   providedIn: 'root'
