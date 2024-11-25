e2cb8ce98f805fbffebb40f4d10689efec33ac75
Samuel Sanchez <125204393+SamuelSV26@users.noreply.github.com>
2024-11-24 19:46:03 -0500

    AuthServices

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,39 +1,43 @@
+import { AuthServiceService } from './../../services/auth-service.service';
 import { Component, OnInit } from '@angular/core';
 import { Token, TokenApi } from '../../models/tokenModel';
 import { CommonServiceService } from '../../services/common-service.service';
 import { AlertServiceService } from '../../common/generalServices/alert-service.service';
 import { environment } from '../../environments/environments';
 import { HttpClientModule } from '@angular/common/http';
+import { ALERTA_TIPO } from '../../common/conts/alerts/sweetAlertConsts';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HttpClientModule], 
+  imports: [HttpClientModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'] 
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
   tokenApi: TokenApi = new TokenApi();
   token: Token = new Token();
 
-  constructor(private commonService: CommonServiceService, 
-              private alertService: AlertServiceService) {}
+  constructor(private commonService: CommonServiceService,
+              private alertService: AlertServiceService,
+              private authService: AuthServiceService) {}
 
   ngOnInit(): void {
   }
 
   getToken() {
     this.tokenApi = {
-      username: "0",
-      password: "abc123"
+        username: "0",
+        password: "abc123"
     };
-    this.commonService.post(`${environment.apiAuthUrl}/login`, this.tokenApi).subscribe((res: Token) => {
-      this.token = res;
-      sessionStorage.setItem('AccessToken', this.token.token);
+    this.authService.getToken(this.tokenApi);
+    const token = this.authService.getAccessToken();
 
-      //Log
-      console.log('Token: ' + this.token.token);
-    });
+    console.log('Token almacenado en sessionStorage:', token);
   }
+
+
 }
+
+
--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,16 +1,38 @@
+import { Token, TokenApi } from './../models/tokenModel';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environments';
+import { ALERTA_TIPO } from '../common/conts/alerts/sweetAlertConsts';
+import { AlertServiceService } from '../common/generalServices/alert-service.service';
+import { CommonServiceService } from './common-service.service';
+import { log } from 'console';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
-
   authUrl = environment.apiAuthUrl;
-  constructor(private http: HttpClient) { }
+  private token: string | null = null;
 
-  login(user: any){
+  constructor(private http: HttpClient
+    , private alertService: AlertServiceService,
+    private commonService: CommonServiceService
+  ) {}
+
+  login(user: any) {
     return this.http.post(`${this.authUrl}/login`, user);
   }
+
+  getToken(tokenApi: TokenApi) {
+    this.commonService.post(`${environment.apiAuthUrl}/login`, tokenApi).subscribe((res: Token) => {
+      sessionStorage.setItem('AccessToken', res.token);
+    });
+  }
+
+  getAccessToken(): string | null {
+    return sessionStorage.getItem('AccessToken');
+  }
 }
+
+
+
